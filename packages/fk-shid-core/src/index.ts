8f1cd356b3d4c892c7800f0ee2a96d38f8ab414e
Sean Long <sean@labrys.io>
2024-10-20 09:58:29 +1000

    restructure core exports

--- packages/fk-shid-core/src/index.ts
+++ packages/fk-shid-core/src/index.ts
@@ -1,13 +1,9 @@
-// Helper utilities
-export * from './utils/canonicalExtractor'
-export * from './utils/linkVendorMatcher'
-export * from './utils/shareIdCleaner'
-export * from './utils/linkDomainVendorMatcher'
-export * from './utils/linkVendorMatcher'
-export * from './utils/processLink'
-
-// Regex exports
-export { vendorLinkCanonicalRegex } from './regex/link-segment-matchers'
-export { vendorLinkDomainRegex } from './regex/domain-matchers'
-// export * from './regex'
-export { mergedVendorRegexes } from './regex'
+export * as domainMatchers from './regex/domain-matchers'
+export * as linkSegmentMatchers from './regex/link-segment-matchers'
+export {
+  getTLDStrictHostPattern,
+  getTLDishHostPattern,
+  numberOfSegments,
+  pathAfterSegmentValue,
+  getEscapedPattern,
+} from './regex/util'
--- packages/fk-shid-core/src/regex/index.ts
+++ packages/fk-shid-core/src/regex/index.ts
@@ -1,16 +1,1 @@
-import { vendorLinkCanonicalRegex } from './link-segment-matchers'
-import * as linkDomainRegex from './domain-matchers'
-
-const mergedVendorRegexes = Object.entries(vendorLinkCanonicalRegex).reduce(
-  (acc, [vendor, regexes]) => {
-    return {
-      ...acc,
-      [vendor]: {
-        canonicalMatchers: regexes,
-      },
-    }
-  },
-  {}
-)
-
-export { vendorLinkCanonicalRegex, linkDomainRegex, mergedVendorRegexes }
+export {}
