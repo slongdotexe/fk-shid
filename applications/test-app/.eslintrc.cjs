dafa794b7bb38b9b2a474d5fc9967e63c81b7f47
Sean Long <sean@labrys.io>
2024-10-19 15:44:52 +1000

    remove test-app

--- applications/test-app/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-/* eslint-env node */
-const { getTsOverride } = require('eslint-config-slongdotexe/getTsOverride.js')
-const path = require('path')
-
-module.exports = {
-  extends: ['eslint-config-slongdotexe/react'],
-  overrides: [getTsOverride(path.join(__dirname, 'tsconfig.json'))],
-}
